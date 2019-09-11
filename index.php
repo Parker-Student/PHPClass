642090d45698352033cc809b6b2a7dfd62b4150e
Foord <700214020@fvtc.edu>
2019-09-10 21:14:25 -0500

    adding files for the first time

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Parker's Homepage</title>
+<link rel="stylesheet" type="text/css" href="css/base.css">
+</head>
+<body>
+    <header><h1>Parker's Website</h1></header>
+    <nav>
+        <ul>
+        <li><a href="/">Homepage</a></li>
+        <li><a href="#">Loop</a></li>
+        <li><a href="#">Countdown</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <img src="images/portriat.jpg" alt="picture of parker"  >
+        <p>My name is Parker Foord. I am a Software Dev student. In my free time I like to sleep, read, and code. I am also a business owner.</p>
+
+    </main>
+    <footer>&copy; 2020 parkerfoord.com</footer>
+</body>
+
+
+
+</html>
