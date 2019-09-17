1638afc0572e4c468fd26a4eccf1c297a4121a58
Foord <700214020@fvtc.edu>
2019-09-16 21:05:06 -0500

    adding files for the first time

--- clock/countdown.php
+++ clock/countdown.php
@@ -6,4 +6,41 @@ $secPerYear = 365 * $secPerDay;
 
 $now = time();
 
+$semester = mktime(23,59,59,12,21,2019);
+
+$seconds = $semester - $now;
+
+$Years = floor($seconds/$secPerYear);
+$seconds = $seconds - ($Years * $secPerYear);
+
+$Days = floor($seconds/$secPerDay);
+$seconds = $seconds - ($Days * $secPerDay);
+
+$Hour = floor($seconds/$secPerHour);
+$seconds = $seconds - ($Hour * $secPerHour);
+
+$Min = floor($seconds/$secPerMin);
+$seconds = $seconds - ($Min * $secPerMin);
+
+
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Parker's Homepage</title>
+    <link rel="stylesheet" type="text/css" href="../css/base.css">
+</head>
+<body>
+<header><?php include '../includes/header.php' ?></header>
+<nav><?php include '../includes/nav.php' ?></nav>
+
+<main>
+<h3>Semester Countdown</h3>
+<p>Years:<?=$Years ?> | Days: <?=$Days ?> | Hours: <?=$Hour ?> | Min: <?=$Min ?> | Seconds: <?=$seconds ?> |</p>
+
+
+</main>
+<footer><?php include '../includes/footer.php' ?></footer>
+</body>
+</html>
--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,5 @@
 <ul>
     <li><a href="/">Homepage</a></li>
     <li><a href="/loops/loops.php">Loop</a></li>
-    <li><a href="#">Countdown</a></li>
+    <li><a href="/clock/countdown.php">Countdown</a></li>
 </ul>
